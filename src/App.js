d262257214143c757bae7bfd2a6b3e06f02920fa
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-09-29 01:34:32 +0500

    Home.js and Home.css Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React from "react";
 import "./App.css";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
     <div className="App">
-      <h1>Hey Clever Programmer fam, let's build the Google Clone🚀</h1>
-
-      {/* Home {the one with the search on}*/}
-
-      {/* SearchPage {the result page} */}
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
