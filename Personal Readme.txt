4c62c93614e153742556811d3914b55e774a3d8b
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-09-28 23:16:19 +0500

    Personal Readme File Added

---
+++ Personal Readme.txt
@@ -0,0 +1,17 @@
+Technologies:
+	Google Custom Search API
+	Material UI
+
+Concepts:
+	Context API (redux)
+	React Router
+	React Hooks (Own Custom Hook)
+
+Using Firebase to setup app to connect to google
+
+Using Firebase hosting to deploy the app
+
+Huge Amount of flexbox.
+
+
+	
--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 
 function App() {
   return (
+    // BEM
     <div className="App">
-      <h1>Hey Clever Programmer fam, let's build the Google Clone 🚀</h1>
+      <h1>Hey Clever Programmer fam, let's build the Google Clone🚀</h1>
+
+      {/* Home {the one with the search on}*/}
+
+      {/* SearchPage {the result page} */}
     </div>
   );
 }
